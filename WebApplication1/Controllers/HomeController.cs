bd25d359785f3cef61548568cc36857c579b41a0
katushka1936@gmail.com <katushka1936@gmail.com>
2019-11-28 08:41:56 +0200

    start

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppStart.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,49 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        List<Client> Clients;
+        List<Employee> Employees;
+        public HomeController()
+        {
+
+
+            using (MyAppContext db = new MyAppContext())
+            {
+                //Client client1 = new Client { FIO = "Иванов Иван Иванович", NameCompany = "Восток", Adres = "Ленина 2-А", City = "Тирасполь", Index = 3300, Tel = 053325687 };
+                //Client client2 = new Client { FIO = "Петров Антон Дмитриевич", NameCompany = "МирПК", Adres = "Восстания 56", City = "Тирасполь", Index = 3300, Tel = 0533569874 };
+                //db.Clients.Add(client1);
+                //db.Clients.Add(client2);
+                //db.SaveChanges();
+                //var clients = db.Clients;
+                Clients = db.Clients.ToList();
+                //Console.WriteLine("Список клиентов:");
+                //foreach (Client u in clients)
+                //{
+                //    Console.WriteLine(u.FIO, u.NameCompany, u.Adres, u.City, u.Index, u.Tel);
+                //}
+                //Employee employee1 = new Employee { FIO = "Пименов Иван Анатольевич", Adres = "Гагарина 2", Tel = 055338725, NumberOrder = 1 };
+                //Employee employee2 = new Employee { FIO = "Агатий Ирина Игоревна", Adres = "25 октября 105", Tel = 07896325, NumberOrder = 2 };
+                //db.Employees.Add(employee1);
+                //db.Employees.Add(employee2);
+                //db.SaveChanges();
+                //var employees = db.Employees;
+                Employees = db.Employees.ToList();
+                //Console.WriteLine("Список Работников:");
+                //foreach (Employee x in employees)
+                //{
+                //    Console.WriteLine(x.FIO, x.Adres, x.Tel, x.NumberOrder);
+                //}
+                //Orders orders1 = new Orders { }
+            }
+        }
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Client()
+        {
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -24,7 +63,7 @@ namespace WebApplication1.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return View(Clients);
         }
     }
 }
---
+++ WebApplication1/Logic/BusinessLogic.cs
@@ -0,0 +1,62 @@
+﻿using ConsoleAppStart.model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStart.Logic
+{
+    class BusinessLogic
+    {
+        MyAppContext db;
+        public BusinessLogic()
+        {
+            db = new MyAppContext();
+        }
+        /// <summary>
+        /// Получить список клиентов
+        /// </summary>
+        /// <param name="Name">Имя компании</param>
+        /// <returns></returns>
+        public List<Client> GetClients(string Name = "")
+        {
+            if (Name == "")
+            {
+                return db.Clients.ToList<Client>();
+            }
+            else
+            {
+                return db.Clients.Where(item => item.NameCompany.IndexOf(Name) > -1).ToList<Client>();
+            }
+        }
+        public string GetStringClient(Client client)
+        {
+            return $"Имя компании:{client.NameCompany}\tИмя клиента:{client.FIO}";
+        }
+        /// <summary>
+        /// Добавить клиента в базу данных
+        /// </summary>
+        /// <param name="client">Клиент</param>
+        public void AddClient(Client client)
+        {
+            //проверка 
+            db.Clients.Add(client);
+            db.SaveChanges();
+        }
+        public void RemoveClient(int id)
+        {
+           Client c1 = db.Clients.Where(x => x.Id == id).First();
+            if (c1 != null)
+            {
+                db.Clients.Remove(c1);
+                db.SaveChanges();
+            }
+
+        }
+       
+
+
+    }
+}

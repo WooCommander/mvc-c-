e074dd9d7e79eb4e9d0fcf090fc95fb6d05efa3d
katushka1936@gmail.com <katushka1936@gmail.com>
2019-12-11 08:52:35 +0200

    ef3

--- WebApplication1/Models/Client.cs
+++ WebApplication1/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace ConsoleAppStart.model
     public class Client
     {
         public int Id { get; set; }
+        [Column("ФИО")]
         public string FIO { get; set; }
         public string NameCompany { get; set; }
         public string Adres { get; set; }
